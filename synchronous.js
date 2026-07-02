4608bdd9a98045c3d2ec2ab490b29e75cac9745f
eden <afomitlahune@gmil$ git config --global user.email afomitlahune@gmail.com>
2026-07-02 21:17:33 +0300

    my synchronous

---
+++ synchronous.js
@@ -0,0 +1,11 @@
+//synchronous
+console.log('macking coffee')
+console.log('waiting for coffee')
+console.log('coffee is ready')
+console.log('drinking coffee')
+//asyncronous
+console.log('ordering coffee')
+setTimeout(() => {
+    console.log ('waiting for coffee')
+    
+},5000)
